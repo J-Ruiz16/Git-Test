88d9b8586e6b4c53a9a43fb0178bf8474f5b5073
Jocelyn Ruiz <jruiz16@ut.utm.edu>
2025-01-22 16:50:05 -0600

    First file of project: adding Hello World program main.cpp.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Jocelyn Ruiz
+ * @date 2025-01-22
+ * @brief simple hello world program
+ * 
+ * Hello World program for first assignment CSCI325 practice with github and linux.
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
